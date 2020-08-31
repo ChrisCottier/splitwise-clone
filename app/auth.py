1cbd591e2448dae79abeeda23a62b60982f21728
chris <cgcottier@gmail.com>
2020-08-30 21:00:04 -0500

    created api post user route

---
+++ app/auth.py
@@ -0,0 +1,10 @@
+from jwt import jwt
+from .config import Config
+
+def create_jwt(user):
+  user_data={
+    "id": user["id"],
+    "email":user["email"],
+  }
+  new_jwt = jwt.encode(user_data, Config.JWT_SECRET, algorithm='HS256')
+  return new_jwt
