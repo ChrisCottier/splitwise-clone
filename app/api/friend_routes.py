2f68ad93ba17fd695e276f11a6784dc0bd4a2be1
Chris Cottier <cgcottier@gmail.com>
2020-09-02 10:30:50 -0500

    working on friend api, dashboard update

--- app/api/friend_routes.py
+++ app/api/friend_routes.py
@@ -10,11 +10,34 @@ friend_routes = Blueprint('friends', __name__)
 
 @friend_routes.route('/<id>')
 def index(id):
-    friends = Friend.query.filter(
-        Friend.user1_id == id or Friend.user2_id == id
-        )
-        .join(
-            User, Friend.user1_id
-        )
-        .all()
-    return jsonify({"friends": [user.to_dict() for user in response]})
+    # friends = Friend.query.filter(
+    #     Friend.user1_id == id or Friend.user2_id == id
+    #     )
+    #     .join(
+    #         User, Friend.user1_id
+    #     )
+    #     .all()
+    # return jsonify({"friends": [user.to_dict() for user in response]})
+
+    print(id)
+    #get list of frienships
+    friendships= Friend.query.filter(Friend.user1_id == id or Friend.user2_id == id).all()
+    print('FRIENDSHIPS',friendships)
+    friendship_dict= [friendship.to_dict() for friendship in friendships]
+    print(friendship_dict)
+
+    # get list of all ids
+    all_ids=[]
+    for friendship in friendship_dict:
+        all_ids.append(friendship["user1_id"])
+        all_ids.append(friendship["user2_id"])
+    print(all_ids)
+    # filter to where id not queried id
+    all_friend_ids=[friend_id for friend_id in all_ids if friend_id != id]
+
+    #query users for 
+    friend_users= User.query.filter(id in all_friend_ids).all()
+    friend_users_dict=[friend.to_dict for friend in friend_users]
+    print(friend_users_dict)
+    return jsonify(friend_users_dict)
+
