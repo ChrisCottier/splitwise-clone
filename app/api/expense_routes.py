5ac6b1c886cb869fadc5fa59f617b1c2c161c8c8
Aaron <aaronkcarpenter@gmail.com>
2020-09-04 11:11:51 -0500

    Fix route indentation of expense routes

--- app/api/expense_routes.py
+++ app/api/expense_routes.py
@@ -53,64 +53,65 @@ def post_expense():
 
   ################################# Expense & Comment Routes ################
 
-  @expense_routes.route('/test')
-  def get_test():
-    return jsonify({ 'message': 'Hello Aaron'})
+## WORKS
+@expense_routes.route('/test')
+def get_test():
+  return jsonify({ 'message': 'Hello Aaron'})
 
-  ## Returns all activity/expenses for a specific user
-  @expense_routes.route('/expenses/all/:id')
-  def all_expenses():
-    all_user_expenses = Expense.query.filter_by(id=Expenses.id).all()
-    return jsonify(all_user_expenses)
-    print(all_user_expenses)
+## Returns all activity/expenses for a specific user
+@expense_routes.route('/expenses/all/:id')
+def all_expenses():
+  all_user_expenses = Expense.query.filter(Expense.title).all()
+  expenses = [all_user_expenses.to_dict() for expense in expenses]
+  # return jsonify(all_user_expenses)
 
-  ## Delete expenses for a specific user
-  @expense_routes.route('/expenses/:id', methods=['DELETE', 'GET'])
-  def delete_expense():
-    delete_me = Expenses(expense=data['expense'])
-    db.session()
-    db.session.delete(delete_me)
-    db.session.commit()
+## Delete expenses for a specific user
+@expense_routes.route('/expenses/:id', methods=['DELETE', 'GET'])
+def delete_expense():
+  delete_me = Expenses(expense=data['expense'])
+  db.session()
+  db.session.delete(delete_me)
+  db.session.commit()
 
-  ## Returns a specific activity/expense for a user
-  def get_expense():
-    user_expense = Expense.query.filter_by(amount = Expenses.amount).all()
-    return jsonify(user_expenses)
+## Returns a specific activity/expense for a user
+def get_expense():
+  user_expense = Expense.query.filter_by(amount = Expenses.amount).all()
+  return jsonify(user_expenses)
 
-  ## Post a new comment to an expense
-  @expense_routes.route('expenses/:id/comments', methods=['POST'])
-  def post_comment():
-    new_comment = Comment(comments=data['comments'])
-    db.session()
-    db.session.add(new_comment)
-    db.session.commit()
-    return jsonify('Your comment was posted')
+## Post a new comment to an expense
+@expense_routes.route('expenses/:id/comments', methods=['POST'])
+def post_comment():
+  new_comment = Comment(comments=data['comments'])
+  db.session()
+  db.session.add(new_comment)
+  db.session.commit()
+  return jsonify('Your comment was posted')
 
-  ## Delete a comment from an expense
-  @expense_routes.routes('expenses/comments/:id', methods=['DELETE'])
-  def delete_comment():
-    delete_comment = Comments(comment=data['comment'])
-    db.session()
-    db.session.delete(delete_comment)
-    db.session.commit()
-    return jsonify('Comment was deleted')
+## Delete a comment from an expense
+@expense_routes.routes('expenses/comments/:id', methods=['DELETE'])
+def delete_comment():
+  delete_comment = Comments(comment=data['comment'])
+  db.session()
+  db.session.delete(delete_comment)
+  db.session.commit()
+  return jsonify('Comment was deleted')
 
-  ## Update the title or amount associated with an amount
-  @expense_routes.route('expenses/:id', methods=['PUT'])
-  def update_title():
-    update_title = Expenses.query.filter_by(id = expense.id).update(expense.title)
-    db.session.commit()
-    return jsonify('Title Updated')
+## Update the title or amount associated with an amount
+@expense_routes.route('expenses/:id', methods=['PUT'])
+def update_title():
+  update_title = Expenses.query.filter_by(id = expense.id).update(expense.title)
+  db.session.commit()
+  return jsonify('Title Updated')
 
-    update_amount = Expenses.query.filter_by(id = expense.id).update(expense.amount)
-    db.session.commit()
-    return jsonify('Amount Updated')
+  update_amount = Expenses.query.filter_by(id = expense.id).update(expense.amount)
+  db.session.commit()
+  return jsonify('Amount Updated')
 
-  ## Return all comments associated with an expense
-  @expense_routes.route('expenses/:id/comments/all')
-  def get_all():
-    get_comment = Comments.query.filter(id = Comment.id == Expense.id).all()
-    return jsonify(get_comment)
+## Return all comments associated with an expense
+@expense_routes.route('expenses/:id/comments/all')
+def get_all():
+  get_comment = Comments.query.filter(id = Comment.id == Expense.id).all()
+  return jsonify(get_comment)
 
-  
-  return jsonify(new_expense)
+
+return jsonify(new_expense)
