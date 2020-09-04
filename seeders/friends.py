468eafeff4dd4f1e96ac72ba4083db4a0098e12a
COrtaDev <COrtaDev@gmail.com>
2020-09-03 21:13:15 -0400

    Seed Data integration

---
+++ seeders/friends.py
@@ -0,0 +1,8 @@
+from app.models.friends import Friend
+
+friends = [
+    Friend(user1_id=1, user2_id=2),
+    Friend(user1_id=1, user2_id=3),
+    Friend(user1_id=1, user2_id=5),
+    Friend(user1_id=1, user2_id=6),
+]
