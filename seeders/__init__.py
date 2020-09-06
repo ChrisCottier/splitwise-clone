76e3ca2883fe8b6dc8f34606a714888ced7ec1f9
COrtaDev <COrtaDev@gmail.com>
2020-09-06 19:43:43 -0400

    Merge Before Pull to update

--- seeders/__init__.py
+++ seeders/__init__.py
@@ -8,8 +8,13 @@ creators = [randint(1, 30) for i in range(30)]
 borrowers = [randint(1, 30) for i in range(30)]
 # payments = [map(lambda amount: amount/2, amounts) for amount in amounts]
 payments = [format(uniform(2.5, 30), '.2f') for i in range(30)]
+
 types = ['profile', 'expense']
 
+male_names = [fake.name_male() for i in range(150)]
+female_names = [fake.name_female() for i in range(150)]
+all_names = male_names + female_names
+print(all_names)
 friends = set()
 while len(friends) < 30:
     friends.add(tuple(sample(range(1, 31), k=2)))
--- seeders/aws.py
+++ seeders/aws.py
@@ -2,7 +2,16 @@ from app.models.images import Image
 
 base_url = 'https://splitwise-clone.s3.us-east-2.amazonaws.com'
 landing_page_url = f'{base_url}/landing'
-asset_url = f'{landing_page_url}/asset{i}.png'
-asset_seeds = [Image(url=asset_url[i], type='asset') for i in range(1, 6)]
+male_url = f'{base_url}/profile/male'
+female_url = f'{base_url}/profile/female'
 
-print(asset_seeds)
+phone_seeds = [Image(
+    url=f'{landing_page_url}/asset{i}.png'.format(i), type='asset') for i in range(1, 6)]
+
+logo_seeds = [Image(url=f'{landing_page_url}/logo-horizontal.png', type='asset'),
+              Image(url=f'{landing_page_url}/logo-square.png',
+                    type='asset')]
+
+male_img_seeds = [Image()] # male images start at 150 and run until 249
+female_img_seeds = [Image()] #
+asset_seeds = [logo_seeds + phone_seeds]
