88246c638a5742d2cb724831d6b5ce81f0fc9282
COrtaDev <COrtaDev@gmail.com>
2020-09-06 01:13:15 -0400

    Full Implementation and Testing Underway

--- client/src/components/Activites.js
+++ client/src/components/Activites.js
@@ -4,7 +4,7 @@ import { Redirect, NavLink } from 'react-router-dom';
 import { apiUrl } from '../config';
 
 import './styles/dashboard.css';
-import { Comment, Debt, Expense, Friend, Group, Transaction } from './sub-components/ActivityTypes'
+import { Comment, Debt, Expense, Group, Transaction } from './sub-components/ActivityTypes'
 import { getRecentActivity } from '../actions/user';
 
 
@@ -25,14 +25,29 @@ const RecentActivity = (props) => {
 
 
     if (activity) {
-        const { comments, debts, expenses, friends, groups, transactions } = activity;
-        const commentComponents = comments.map((comment) => <Comment key={comment.id} comment={comment} />)
-        const debtComponents = comments.map((comment) => <Debt key={debt.id} debt={debt} />)
+        let currentUser = { userId, name };
+        const { comments, debts, expenses, groups, transactions } = activity;
+        const commentComponents = comments.map((comment) => <Comment key={comment.id} comment={comment} />);
+        const debtComponents = debts.map((debt) => <Debt key={debt.id} debt={debt} currentUser={currentUser} />);
+        const expenseComponents = expenses.map((expense) => <Expense key={expense.id} expense={expense} />);
+        const groupComponents = groups.map((group) => <Group key={group.id} group={group} currentUser={currentUser} />);
+        const transactionComponents = transactions.map((transaction) => <Transaction key={transaction.id} transaction={transaction} currentUser={currentUser} />)
         return (
             <>
                 <h1> Recent Activity </h1>
-                <div>
-                {commentComponents}
+                <div style={{ width: '600px' }}>
+                    <div>
+                        {commentComponents}
+                    </div>
+                    <div>
+                        {debtComponents}
+                    </div>
+                    <div>
+                        {expenseComponents}
+                    </div>
+                    <div>
+                        {groupComponents}
+                    </div>
                 </div>
             </>
         );
@@ -40,108 +55,6 @@ const RecentActivity = (props) => {
         return null;
     }
 
-
-
-    // useEffect(() => {
-
-    //     const getActivity = async () => {
-    //         // const res = await fetch(`${apiUrl}/activities/${userId}`);
-    //         const res = await fetch(`${apiUrl}/activities/1`);
-    //         const data = await res.json();
-    //         console.log(data)
-    //         setRecentActivity(data);
-    //         // const { comments, debts, expenses, friends, groups, transactions } = recentActivity;
-    //         // const allActivity = [comments, debts, expenses, friends, groups, transactions];
-    //     }
-    //     getActivity();
-    //     // getActivity.preventDefault();
-    // }, []);
-
-
-    // console.log(recentActivity);
-    // console.log(allActivity);
-
-    // if (true) {
-    //     return (
-    //         <>
-    //             <h1> Recent Activity </h1>
-    //             <p>No Recent Activiy</p>
-    //         </>
-    //     )
-    // } else {
-    //     return (
-    //         <>
-    //             <h1> Recent Activity </h1>
-    //         </>
-    //     );
-
-
-
-
-
-
 };
 
-
 export default RecentActivity;
-// export const ActivityHeader = (props) => {
-//     const { title } = props;
-
-//     return (
-//         //   just return a header
-//         <div className="dashboard-header">
-//             <div className="title is-5">{title}</div>
-//         </div>
-//     )
-// }
-
-// const commentComponent = () => {
-
-// }
-
-// const debtComponent = () => {
-
-// }
-
-// const expenseComponent = () => {
-
-// }
-
-// const friendComponent = () => {
-
-// }
-
-// const groupComponent = () => {
-
-// }
-
-// const transactionComponent = () => {
-
-// }
-
-
-// const DashboardCenter = () => {
-//     const dispatch = useDispatch()
-//     const [recentActivity, setRecentActivity] = useState([])
-
-//     const { userId, token } = useSelector(state => state.auth);
-//     const { comments, debts, expenses, friends, groups, transactions } = useSelector(state => state.data)
-
-//     useEffect(() => {
-//         setRecentActivity(dispatch(getRecentActivity(userId)))
-//     }, [])
-
-//     if (!token) {
-//         return <Redirect to="/"></Redirect>
-//     }
-
-//     return (
-//         <>
-//             <ActivityHeader title={'Recent Activity'} />
-//         </>
-//     )
-// }
-
-// const Dashboard = () => {
-//     return <PageLayout center={<DashboardCenter></DashboardCenter>}></PageLayout>
-// }
--- client/src/components/sub-components/ActivityTypes.js
+++ client/src/components/sub-components/ActivityTypes.js
@@ -1,30 +1,89 @@
 import React from 'react';
 
 export const Comment = (props) => {
-    const {comment} = props;
+    const { id, commentor, expense, message, created_at } = props.comment;
+    // I would like the messages to be limited to 75 characters only.
+    let trimmedMessage = message.substring(0, 75) + "...";
+    if (message.length <= 75) trimmedMessage = message;
+    return (  // We render the component with inline style for now.
+        <>
+            <span key={id} style={{ display: 'flex', flexDirection: 'row' }}>
+                <div style={{ minWidth: '10%' }}>
+                    <img src={commentor.image_url.url} style={{ height: '60px', width: '60px' }}></img>
+                </div>
+                <div>
+                    <p style={{ wordWrap: 'break-word' }}><strong>{commentor.name}</strong> commented on <strong>"{expense.title}":</strong> "{trimmedMessage}"</p>
+                    <p>{created_at}</p>
+                </div>
+            </span>
+        </>
+    )
+}
+
+export const Debt = (props) => {
+    const { currentUser } = props;
+    const { id, borrower_id, amount, created_at, lender } = props.debt;
+    // If the currunt user in not the borrower, they do not have a debt associated with the current expense
+    if (currentUser !== borrower_id) return;
     return (
-        <p key={comment.id}><strong>{comment.commentor.name}</strong> commented on <strong>"{comment.expense.title}":</strong> "{comment.message}"</p>
+        <>
+            <span key={id} style={{ display: 'flex', flexDirection: 'row' }}>
+                <div>
+                    <img src={lender.image_url.url} style={{ height: '60px', width: '60px' }}></img>
+                </div>
+                <div>
+                    <p><strong>{lender.name}</strong> added <strong>"{title}."</strong></p>
+                    <p>{creator.name} paid ${amount}</p>
+                    <p>{created_at}</p>
+                </div>
+            </span>
+        </>
+    )
+
+}
+
+export const Expense = (props) => {
+    const { id, creator, title, amount, created_at, image_url } = props.expense;
+    return (
+        <>
+            <span key={id} style={{ display: 'flex', flexDirection: 'row' }}>
+                <div>
+                    <img src={image_url.url} style={{ height: '60px', width: '60px' }}></img>
+                </div>
+                <div>
+                    <p><strong>{creator.name}</strong> added <strong>"{title}."</strong></p>
+                    <p>{creator.name} paid ${amount}</p>
+                    <p>{created_at}</p>
+                </div>
+            </span>
+        </>
+    );
+};
+
+export const Group = (props) => {
+    const { currentUser } = props;
+    const { id, name, user1_id, created_at } = props.group
+
+    if (currentUser.userId === user1_id) {
+        return (
+            <div key={id}>
+                <p><strong>{currentUser.name}</strong> created the group <strong>"{name}".</strong></p>
+                <p>{created_at}</p>
+            </div>
         )
-    }
-
-    export const Debt = (props) => {
-        const {debt} = props;
-
+    } else {
+        return (
+            <div key={id}>
+                <p><strong>{currentUser.name}</strong> joined the group <strong>"{name}."</strong></p>
+                <p>{created_at}</p>
+            </div>
+        );
+    };
 }
 
-export const Expense = () => {
-
-}
-
-export const Friend = () => {
-
-}
-
-export const Group = () => {
-
-}
-
-export const Transaction = () => {
+export const Transaction = (props) => {
+    const { currentUser } = props;
+    const { id, amount, created_at, } = props.transaction
 
 }
 
@@ -32,7 +91,6 @@ export default {
     Comment,
     Debt,
     Expense,
-    Friend,
     Group,
     Transaction
 };
