0b9844eb8ba03a934d90e08ff81313cff2bdb69e
Chris <cgcottier@gmail.com>
2020-09-29 15:43:55 -0500

    login page facelift, validations, proper routing

--- app/auth.py
+++ app/auth.py
@@ -1,20 +1,45 @@
 import jwt
 from .config import Config
+import bcrypt
+
+from app.models.users import User
+
 
 def create_jwt(user):
-  user_data={
-    "id": user["id"],
-    "email": user["email"],
-    "name": user["name"]
-  }
-  new_jwt = jwt.encode(user_data, Config.JWT_SECRET, algorithm='HS256')
-  return new_jwt
+    user_data = {
+        "id": user["id"],
+        "email": user["email"],
+        "name": user["name"]
+    }
+    new_jwt = jwt.encode(user_data, Config.JWT_SECRET, algorithm='HS256')
+    return new_jwt
+
 
 def validate_jwt(request):
-  auth_header= request.headers['Authorization']
-  token=auth_header[7:]
-  try:
-    payload = jwt.decode(token, Config.JWT_SECRET)
-    return {"user": payload, "token":token}
-  except:
-    return False
+    auth_header = request.headers['Authorization']
+    token = auth_header[7:]
+    try:
+        payload = jwt.decode(token, Config.JWT_SECRET)
+        return {"user": payload, "token": token}
+    except:
+        return False
+
+
+def validate_log_in(data):
+    email = data['email']
+    password = data['password']
+
+    user_exists = User.query.filter(User.email == email).first()
+    print('user exists', user_exists)
+    if not user_exists:
+        return ['There is no account associated with that email.']
+
+    hashed_password = user_exists.hashed_password
+    if not bcrypt.checkpw(password.encode(), hashed_password.encode()):
+        return ['Invalid password for that account.']
+
+    return []
+
+    # email not used
+    # passwords match
+    # lengths
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
+import "./styles/login-signup.css";
 import { login } from "../actions/auth";
 import { SplashNav } from "./Splash";
 
 const Login = () => {
-  const { token } = useSelector((state) => state.auth);
+  const { token, errors } = useSelector((state) => state.auth);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
@@ -41,52 +42,65 @@ const Login = () => {
     <>
       <SplashNav></SplashNav>
       <div className="container is-widescreen">
-        <div className="control">
-          <p>WELCOME TO SPLITWISE</p>
+        <div id="log-in-container">
+          <h1 className="welcome-header title is-4">WELCOME TO SPLITWISE</h1>
+          <ul id="validations-container">
+            {errors.length > 0 ? (
+              errors.map((error, ind) => <li key={ind}>{error}</li>)
+            ) : (
+              <></>
+            )}
+          </ul>
+          <form action="sign-up" method="POST" onSubmit={submitLogin}>
+            <div className="field">
+              <label className="label">Email address</label>
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="email"
+                  autoComplete="email"
+                  name="email"
+                  required
+                  value={email}
+                  onChange={setField}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="password"
+                  autoComplete="current-password"
+                  name="password"
+                  required
+                  value={password}
+                  onChange={setField}
+                />
+              </div>
+            </div>
+            <div className="field is-grouped">
+              <div className="control">
+                <button
+                  id="login-button"
+                  className="button is-link"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </div>
+              <div className="control">
+                <button
+                  className="button is-link is-light"
+                  onClick={loginDemoUser}
+                >
+                  Login as Demo User
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <form action="sign-up" method="POST" onSubmit={submitLogin}>
-          <div className="field">
-            <label className="label">Email address</label>
-            <div className="control">
-              <input
-                className="input is-info"
-                type="email"
-                autoComplete="email"
-                name="email"
-                required
-                value={email}
-                onChange={setField}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Password</label>
-            <div className="control">
-              <input
-                className="input is-info"
-                type="password"
-                autoComplete="current-password"
-                name="password"
-                required
-                value={password}
-                onChange={setField}
-              />
-            </div>
-          </div>
-          <div className="field is-grouped">
-            <div className="control">
-              <button className="button is-link">Login</button>
-            </div>
-            <div className="control">
-              <button
-                className="button is-link is-light"
-                onClick={loginDemoUser}
-              >
-                Login as Demo User
-              </button>
-            </div>
-          </div>
-        </form>
       </div>
     </>
   );
